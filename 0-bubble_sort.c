119ca016e5eafcf0fd905df4c8fc8def98b562bd
ThomasOselu <thomasoselu1@gmail.com>
2023-04-23 00:16:37 -0700

    sorting algo

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "sort.h"
+/**
+ * bubble_sort -sorts an array of integers in ascending
+ * order using the Bubble sort
+ * @array: the array containing the elements to order
+ * @size: the number of elements in the array
+ *
+ * Return: array in ascending order
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+C - Sorting algorithms & Big O
+Project is done in teams of 2 people (your team: Wonderful Adeyemo, Thomas Odhiambo)
+
+Resources
+Read or watch:
+
+Sorting algorithm
+Big O notation
+Sorting algorithms animations
+15 sorting algorithms in 6 minutes (WARNING: The following video can trigger seizure/epilepsy. It is not required for the project, as it is only a funny visualization of different sorting algorithms)
+CS50 Algorithms explanation in detail by David Malan
+All about sorting algorithms
