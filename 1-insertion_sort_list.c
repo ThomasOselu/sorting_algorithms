6cd0255591e2870c8ed3264adbda5de075084271
Dev-wonderful <adeonederful20@gmail.com>
2023-04-25 14:19:24 +0000

    ins

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,9 +6,38 @@
  *
  * Return: void
  */
-void insertion_sort_list(listint **list)
+void insertion_sort_list(listint_t **list)
 {
-	listint *head = *list;
+	listint_t *head, *node;
+	int length = 0;
+	int index;
+
+	head = *list;
+	node = head;
+	while(node != NULL)
+	{
+		length++;
+		node = node->next;
+	}
+
+	current = head->next /*getting the second node, array[1]*/
+
+	for (index = 0; index < length; index++)
+	{
+		prev_node = current->prev;
+		temp = current;
+		while (prev_node != NULL)
+		{
+			if (current->n < prev_node->n)
+			{
+				prev_node->next = temp->next;
+				temp->next->prev = prev_node;
+			}
+			temp = prev_node;
+			prev_node = prev_node->prev;
+		}
+		current = current->next
+	}
 
 	while (head != NULL)
 	{
