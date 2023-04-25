93e77a79df8bf1698dfb16181060adb39e3f560d
Dev-wonderful <adeonederful20@gmail.com>
2023-04-25 12:52:42 +0000

    ins

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,18 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sort the list in ascending order
+ * @list: a pointer to the head
+ *
+ * Return: void
+ */
+void insertion_sort_list(listint **list)
+{
+	listint *head = *list;
+
+	while (head != NULL)
+	{
+		node = head;
+		node->
+	}
+}
